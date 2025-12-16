6fa3886860fceeeebccb07f809e0c90eed7e6219
milbry <milbecerramurrugarra@crackthecode.la>
2025-12-16 17:20:47 -0500

    bsqwe

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -1,13 +1,25 @@
 import { useEffect, useState } from 'react';
+
 import { onAuthStateChanged } from 'firebase/auth';
+
 import { auth } from '../firebase.js';
 
+
+
 export function useAuthStateLocal() {
+
   const [user, setUser] = useState(null);
+
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+
     const unsub = onAuthStateChanged(auth, u => { setUser(u); setLoading(false); });
+
     return () => unsub();
+
   }, []);
+
   return { user, loading };
+
 }
